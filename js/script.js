00d0cdb5772c269c20169499e5ad7924b6dd68a6
Rosamund Pearce <rosamundpearce@live.com>
2019-01-16 16:22:16 +0000

    update variable name

--- js/script.js
+++ js/script.js
@@ -18,6 +18,8 @@ $("#chart-container").css("height", (textHeight + placeholderHeight));
 let chartFromTop = $('#section-1').height() + $('#section-2').height();
 $("#chart-container").css("top", (chartFromTop + 10));
 
+let sectionName;
+
 $( document ).ready(function() {
 
     // scroll to top on window reload
@@ -26,7 +28,7 @@ $( document ).ready(function() {
     $textSection.each(function(){
 
         let _this = this;
-        let sectionName = $(this).attr('id');
+        sectionName = $(this).attr('id');
         let graphFunction = "triangle" + sectionName;
 
         var inViewBottom = new Waypoint({
